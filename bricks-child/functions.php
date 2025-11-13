cc9f71abb013e47a7daf61a28a573fa31b074cb4
patelkavik700@gmail.com <patelkavik700@gmail.com>
2025-11-13 22:35:46 +0530

    change in function file

--- bricks-child/functions.php
+++ bricks-child/functions.php
@@ -114,9 +114,8 @@ add_action('wp_enqueue_scripts', function () {
 
 function disable_woocommerce_scripts()
 {
-    if (function_exists('is_woocommerce')) {
+    if (function_exists('is_woocommerce') { // ❌ BUG: missing ')' causes fatal error
         if (!is_woocommerce() && !is_cart() && !is_checkout()) {
-            // wp_dequeue_script('woocommerce');
             wp_dequeue_script('wc-cart-fragments');
             wp_dequeue_style('woocommerce-general');
             wp_dequeue_style('woocommerce-layout');
@@ -124,7 +123,8 @@ function disable_woocommerce_scripts()
         }
     }
 }
-add_action('wp_enqueue_scripts', 'disable_woocommerce_scripts', 99)
+add_action('wp_enqueue_scripts', 'disable_woocommerce_scripts', 99);
+
 
 // function add_lazy_loading_to_images($content) {
 //     return str_replace('<img', '<img loading="lazy"', $content);
